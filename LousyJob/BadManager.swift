27ba7a9300752f9ce8b2d3ff3503ee746e1ccd69
Jacob Williams <jacobaw@gmail.com>
2021-02-27 10:06:54 -0800

    show job status

--- LousyJob/BadManager.swift
+++ LousyJob/BadManager.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import SwiftUI
 
 class BadManager {
     var config: ConfigRoot
@@ -10,6 +11,7 @@ class BadManager {
     var menuItemCopyPid: NSMenuItem
     var menuItemViewStdout: NSMenuItem
     var menuItemViewStderr: NSMenuItem
+    var lastinc: Incident?
     
     var fout: FileHandle?
     var ferr: FileHandle?
@@ -77,8 +79,24 @@ class BadManager {
         }
     }
     
-    func reload() {
+    func refresh() {
         recheck()
+        menuItem.image = NSImage(systemSymbolName: "questionmark.circle", accessibilityDescription: "unclear")
+        if process != nil {
+            menuItem.image = NSImage(systemSymbolName: "ellipsis.rectangle", accessibilityDescription: "running")
+        } else if let inc = lastinc {
+            if inc.kind == .finished {
+                if inc.exitCode == 0 {
+                    menuItem.image = NSImage(systemSymbolName: "checkmark", accessibilityDescription: "succeeded")
+                } else {
+                    menuItem.image = NSImage(systemSymbolName: "xmark.circle", accessibilityDescription: "failed")
+                }
+            }
+        }
+    }
+    
+    func reload() {
+        refresh()
         menuItem.title = configJob.title
     }
     
@@ -89,44 +107,37 @@ class BadManager {
         process = Process()
         process!.launchPath = configJob.executable
         process!.arguments = configJob.arguments
-        process!.terminationHandler = { p in self.recheck() }
+        process!.terminationHandler = { p in self.refresh() }
         let foutpath = stdoutLogPath()
         if !FileManager.default.fileExists(atPath: foutpath) {
             FileManager.default.createFile(atPath: foutpath, contents: nil, attributes: nil)
         }
         fout = FileHandle(forWritingAtPath: foutpath)
-        if #available(OSX 10.15.4, *) {
-            try! fout!.seekToEnd()
-        }
+        try! fout!.seekToEnd()
         process!.standardOutput = fout!
         let ferrpath = stderrLogPath()
         if !FileManager.default.fileExists(atPath: ferrpath) {
             FileManager.default.createFile(atPath: ferrpath, contents: nil, attributes: nil)
         }
         ferr = FileHandle(forWritingAtPath: ferrpath)
-        if #available(OSX 10.15.4, *) {
-            try! ferr!.seekToEnd()
-        }
+        try! ferr!.seekToEnd()
         process!.standardError = ferr!
         process!.launch()
         
         let inc = Incident(kind: .started, date: .init(), jobId: configJob.id, pid: process!.processIdentifier, exitCode: nil, executable: process!.launchPath, arguments: process!.arguments)
         record(incident: inc)
         
-        recheck()
+        refresh()
     }
     
     func record(incident: Incident) {
+        lastinc = incident
         let fpath = lousyjobLogPath()
         if !FileManager.default.fileExists(atPath: fpath) {
             FileManager.default.createFile(atPath: fpath, contents: nil, attributes: nil)
         }
         if let flj = FileHandle(forWritingAtPath: fpath) {
-            if #available(OSX 10.15.4, *) {
-                try! flj.seekToEnd()
-            } else {
-                flj.seekToEndOfFile()
-            }
+            try! flj.seekToEnd()
             flj.write(incident.toJSON())
             flj.write("\n".data(using: .utf8)!)
             try? flj.close()
